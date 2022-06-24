585b3cfc60a8074d7f846007134cbc1f934c0b9b
Jaron <jjkenyon777@gmail.com>
2022-06-24 10:24:50 -0700

    Context API

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -36,6 +37,9 @@ const Login = (props) => {
   const [passwordState, dispatchPassword] = useReducer(passwordReducer,
     {value: '', isValid: null});
 
+    const authCtx = useContext(AuthContext);
+
+    // destructure email/passwordstate.isValid into email/passwordIsValid
     const { isValid: emailIsValid } = emailState; 
     const { isValid: passwordIsValid } = passwordState;
   // form validity effect ran everytime the 
@@ -85,7 +89,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
